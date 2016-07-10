81b4a1ee833f1642932b3ad53699b69f178c9487
michaelsimon <michael.simon@gmail.com>
2016-07-10 14:13:16 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var tacoIngredients = {};
+
+function addIngredient(tacoIngredients, ingredient, amount) {
+  tacoIngredients[ingredient] = amount;
+  return tacoIngredients;
+}
+
+function removeIngredient(tacoIngredients, ingredient) {
+  delete tacoIngredients[ingredient];
+  return tacoIngredients;
+}
+
+function updateIngredient(tacoIngredients, ingredient, amount) {
+  tacoIngredients[ingredient] = amount;
+  return tacoIngredients;
+}
+
+function readRecipe(tacoIngredients) {
+  for (var ingredient in tacoIngredients) {
+    console.log("this recipe calls for " + tacoIngredients[ingredient] + " of " + ingredient);
+  }
+}
